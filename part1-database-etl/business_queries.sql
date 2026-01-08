a5b197d155855a0a4535073ea571b60089cdce65
KOTHKAI7 <84524179+KOTHKAI7@users.noreply.github.com>
2026-01-08 17:08:17 +0530

    Update business_queries.sql

--- part1-database-etl/business_queries.sql
+++ part1-database-etl/business_queries.sql
@@ -1,3 +1,7 @@
+-- Query 1: Customer Purchase History
+-- Business Question: Generate a detailed report showing each customer's name, email, total number of orders placed, and total amount spent.
+-- Include only customers who have placed at least 2 orders and spent more than ₹5,000. Order by total amount spent desc.
+
 SELECT
     CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
     c.email,
